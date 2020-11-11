e0129d4e623e410902a3c64e364df0347aea9790
shravanthi94 <b.s.shravanthi94@gmail.com>
2020-11-11 11:54:13 -0800

    Student signup modified route

--- backend/routes/student/resume.js
+++ backend/routes/student/resume.js
@@ -1,8 +1,9 @@
 "use strict"
-const app = require("../../app");
 const express = require("express");
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 
 
-router.post("/student/resume", async (req, res) => {}
+router.post("/student/resume", async (req, res) => {});
+
+module.exports = router;
