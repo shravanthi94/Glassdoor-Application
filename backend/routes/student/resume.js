e9607b430c4a531f1b0991be169f0169983bce2c
AnudeepCHS <srinivasanudeep.chitta@sjsu.edu>
2020-11-11 11:16:52 -0800

    creating route and model foldrs

---
+++ backend/routes/student/resume.js
@@ -0,0 +1,8 @@
+"use strict"
+const app = require("../../app");
+const express = require("express");
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+
+
+router.post("/student/resume", async (req, res) => {}
