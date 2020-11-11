c6399106cd4c814f7cc985e3a68b0cb9d5a2a0ce
shravanthi94 <b.s.shravanthi94@gmail.com>
2020-11-11 09:28:47 -0800

    Initial backend setup

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const app = require('./app');
+//  Connect database
+const connectDB = require('./config/db');
+
+connectDB();
+
+app.get('/', (req, res) => {
+  res.send('hello');
+});
+
+//  Connection to a port
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Application started listening to port ${PORT} successfully.`);
+});
