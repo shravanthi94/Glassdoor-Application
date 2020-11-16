fce177d3368a144471a7255328cc3cfd3f68f136
harika517 <hharika38@gmail.com>
2020-11-15 18:20:00 -0800

    frontend react route and redux setup

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React, {Fragment} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import './App.css';
+
+// Redux
+
+import { Provider } from 'react-redux';
+import store from './store';
+
+const App = () => 
+<Provider store={store}>
+    <Router>
+    <Fragment>
+      <h1> Glassdoor </h1>
+    </Fragment>
+    </Router>
+</Provider>
+
+
+export default App;
