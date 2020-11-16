eb2c6ec1518428bd0bc4c501cafba21fb3bc0ae0
BhavanaBS <bhavana.bs1566@gmail.com>
2020-11-16 14:14:58 -0800

    base kafka setup
    base kafka setup

---
+++ kafka-backend/models/CompanyModel.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+
+const CompanySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    profilePic: {
+        type: String
+    },
+    ceoName: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    website: {
+        type: String,
+        required: true
+    },
+    size: {
+        type: String
+    },
+    type: {
+        type: String
+    },
+    revenue: {
+        type: String
+    },
+    headquarters: {
+        type: String
+    },
+    industry: {
+        type: String
+    },
+    founded: {
+        type: String
+    },
+    mission: {
+        type: String,
+        required: true
+    },
+    images: [{
+        type: String
+    }],
+    date: { type: Date, default: Date.now() },
+});
+
+const Company = mongoose.model('company', CompanySchema);
+module.exports = Company;
---
+++ kafka-backend/models/JobPostingModel.js
@@ -0,0 +1,75 @@
+const mongoose = require('mongoose');
+
+const JobPostingSchema = new mongoose.Schema({
+    company: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'company',
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    responsibilities: [{
+        type: String,
+        required: true
+    }],
+    qualifications: [{
+        type: String,
+        required: true
+    }],
+    industry: {
+        type: String,
+        required: true
+    },
+    country: {
+        type: String
+    },
+    Remote: {
+        type: Boolean,
+        required: true
+    },
+    inPerson: {
+        type: Boolean,
+        required: true
+    },
+    street: {
+        type: String
+    },
+    city: {
+        type: String
+    },
+    state: {
+        type: String
+    },
+    zip: {
+        type: String
+    },
+    date: { type: Date, default: Date.now() },
+    applicants: [{
+        student: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'student'
+        },
+        resume: {
+            type: String
+        },
+        coverLetter: {
+            type: String
+        },
+        applicantStatus: {
+            type: String
+        },
+        appliedDate: {
+            type: Date,
+            default: Date.now()
+        },
+    }]
+
+});
+
+const Jobposting = mongoose.model('jobposting', JobPostingSchema);
+module.exports = Jobposting;
