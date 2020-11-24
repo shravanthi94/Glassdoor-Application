d8aee6d20afbb19406a817eafd7a1b8e302afdaf
harika517 <hharika38@gmail.com>
2020-11-24 15:24:41 -0800

    Employer Landing Page

--- frontend/src/components/Employer/Landing.js
+++ frontend/src/components/Employer/Landing.js
@@ -1,11 +1,20 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+/* eslint-disable jsx-a11y/alt-text */
+import React, {Fragment} from 'react'
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import EmpNav from './EmpNav';
+import '../CSS/EmployerLanding.css';
 
 const Landing = props => {
     return (
-        <div>
-            Company Landing Navbar
-        </div>
+        <Fragment>
+            <EmpNav/>
+            <div className="container">
+                <img src="https://www.glassdoor.com/employers/app/uploads/sites/2/2020/11/illustration_home_your-employer-brand-large-3.svg"/>
+            </div> 
+           
+        </Fragment>
+        
     )
 }
 
