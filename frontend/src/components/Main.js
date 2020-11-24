749b65dffba0bec2e408b737fa3af57c4017b2b6
Shravanthi <57304916+shravanthi94@users.noreply.github.com>
2020-11-23 21:29:01 -0800

    Added navbar

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,16 +1,17 @@
-import React, {Component} from 'react';
-import {Route} from 'react-router-dom';
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import Navbar from './Navbar';
 
 //Create a Main Component
 class Main extends Component {
-    render(){
-        return(
-            <div>
-                {/*Render Different Component based on Route*/}
-                {/* <Route path="/" component={navbar}/>*/}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        {/*Render Different Component based on Route*/}
+        <Route path='/' component={Navbar} />
+      </div>
+    );
+  }
 }
 //Export The Main Component
 export default Main;
