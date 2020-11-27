d69ff779bf3d7fc26b68d1fd0a198770ffdd8b34
BhavanaBS <bhavana.bs1566@gmail.com>
2020-11-27 00:30:32 -0800

    admin analytics
    admin analytics:
    Top 5 students based on total accepted reviews made.
    Top 10 CEO’s based on rating.

--- backend/routes/admin/analytics.js
+++ backend/routes/admin/analytics.js
@@ -1,7 +1,4 @@
 // No of reviews per day.
-// Top 5 company based on average rating.
-// Top 5 students based on total accepted reviews made. 
-// Top 10 CEO’s based on rating.
 // Top 10 company based on viewed per day.
 
 const express = require('express');
@@ -9,7 +6,6 @@ const mongoose = require('mongoose');
 
 const router = express.Router();
 const Company = require('../../models/CompanyModel');
-const JobPostings = require('../../models/JobPostingModel');
 const Reviews = require('../../models/ReviewModel');
 const Student = require('../../models/StudentModel');
 
@@ -55,10 +51,10 @@ router.get('/most-reviewed-company', adminCheckAuth, async(req, res) => {
                 "$project": {
                   "_id": 0,
                   "company_id": 1,
-                  "company.name": 1,
+                  "name": '$company.name',
                   "count": 1
                 }
-              }
+            }
         ]);
         query.limit(parseInt(req.query.limit));
         query.exec((err, mostReviewedCompany) => {
@@ -92,6 +88,98 @@ router.get('/best-average-rating', adminCheckAuth, async(req, res) => {
                 return res.status(500).send('Server Error: Database');
             } else if(bestAverageRating){
                 return res.status(200).json({bestAverageRating:bestAverageRating});
+            }
+        });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error: Database');
+    }
+});
+
+// @route  GET /admin/analytics/top-ceo
+// @Desc   Get a list of all top companies having the best ceo based rating from the database
+// @access Private
+router.get('/top-ceo', adminCheckAuth, async(req, res) => {
+    try {
+        console.log(req.query.limit);
+        console.log("Get a list of all top companies having the best ceo based rating from the database");
+        const query = Company.find();
+        query.select('_id ceoApprovalRating ceoName name');
+        query.sort({ceoApprovalRating:-1});
+        query.limit(parseInt(req.query.limit));
+        query.exec((err, bestCeoRating) => {
+            if (err) {
+                console.error(err.message);
+                return res.status(500).send('Server Error: Database');
+            } else if(bestCeoRating){
+                return res.status(200).json({bestCeos:bestCeoRating});
+            }
+        });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error: Database');
+    }
+});
+
+// @route  GET /admin/analytics/top-student-reviewer
+// @Desc   Get a list of top students based on total accepted reviews made from the database
+// @access Private
+router.get('/top-student-reviewer', adminCheckAuth, async(req, res) => {
+    try {
+        console.log(req.query.limit);
+        console.log("Get a list of top students based on total accepted reviews made from the database");
+        // const query = Reviews.find({approvalStatus:{ $eq: 'approved' }});
+        // query.select('student');
+        // query.populate({ path: 'student', select: 'name' });
+        // query.count(student._id);
+        // query.sort({ceoApprovalRating:-1});
+        // query.limit(parseInt(req.query.limit));
+
+        const query = Reviews.aggregate([
+            { 
+                "$match": { "approvalStatus": { $eq: 'approved' } } 
+            },
+            {  
+                "$group": {
+                    "_id": "$student",
+                    "student_id": {
+                        "$first" : "$student"
+                    },
+                    "count": { "$sum": 1 }
+                } 
+            },
+            { 
+                "$sort": { 
+                    "count": -1 
+                } 
+            },
+            {
+                "$lookup": {
+                    "from": "students",
+                    "localField": "student_id",
+                    "foreignField": "_id",
+                    "as": "student"
+                }
+            },
+            {
+                "$unwind": "$student"
+            },
+            {
+                "$project": {
+                  "_id": 0,
+                  "student_id": 1,
+                  "name": '$student.name',
+                  "count": 1
+                }
+            }
+        ]);
+        query.limit(parseInt(req.query.limit));
+        query.exec((err, mostStudentReviews) => {
+            if (err) {
+                console.error(err.message);
+                return res.status(500).send('Server Error: Database');
+            } else if(mostStudentReviews){
+                return res.status(200).json({topStudentReviewers:mostStudentReviews});
             }
         });
     } catch (err) {
