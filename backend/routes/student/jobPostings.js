c6509c09c5b3b9f68e28adcfb327e93bd74fe3f9
AnudeepCHS <srinivasanudeep.chitta@sjsu.edu>
2020-11-27 10:38:35 -0800

    withdraw application backend fixed

--- backend/routes/student/jobPostings.js
+++ backend/routes/student/jobPostings.js
@@ -4,13 +4,14 @@ const router = express.Router();
 const { checkAuth } = require('../../middleware/studentAuth');
 const Jobposting = require('../../models/JobPostingModel');
 
-router.post("/apply/:id", checkAuth,async (req, res) => {
+router.post("/apply/:id",async (req, res) => {
     let applicant ={...req.body}
-
+    console.log("to apply:",applicant)
     try {
         console.log("job_id to apply: ", req.params.id);
         let appliedJob = await Jobposting.findOneAndUpdate({ _id: req.params.id }, { $push: { applicants: applicant } }, { new: true });
         if (appliedJob){
+            console.log("after apply:",appliedJob.applicants)
             return res.status(200).json({ msg: 'application posted successfully' });
         }
         else {
@@ -56,12 +57,15 @@ router.get("/applied",checkAuth, async (req, res) => {
 
 });
 
-router.post("/withdraw/:id", checkAuth,async (req, res) => {
+router.post("/withdraw/:id",async (req, res) => {
     
     try {
-        console.log("job_id to apply: ", req.params.id);
+        console.log("job_id to withdraw: ", req.params.id);
+        console.log("student_id to withdraw: ", req.body.student);
     let job = await Jobposting.findOne({ _id: req.params.id })
-        job.applicants.pull({ student: req.body.student});
+    console.log("before filter",job.applicants)
+        job.applicants = job.applicants.filter(data => data.student != req.body.student );;
+        console.log("AFTER filter:",job.applicants)
         job.save();
         res.status(200).json('application withdrawn');
         
