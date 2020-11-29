0b8a117a1d3f2d6574c39786f199efc22bc33f02
Shravanthi <57304916+shravanthi94@users.noreply.github.com>
2020-11-29 12:10:28 -0800

    Added images query for company and modified company model

--- backend/models/CompanyModel.js
+++ backend/models/CompanyModel.js
@@ -1,179 +1,198 @@
 const mongoose = require('mongoose');
 
 const CompanySchema = new mongoose.Schema({
-    name: {
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  profilePic: {
+    type: String,
+    default: 'default',
+  },
+  logo: {
+    type: String,
+    default: 'default',
+  },
+  ceoName: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  website: {
+    type: String,
+  },
+  size: {
+    type: String,
+  },
+  type: {
+    type: String,
+  },
+  revenue: {
+    type: String,
+  },
+  headquarters: {
+    type: String,
+  },
+  mission: {
+    type: String,
+  },
+  industry: {
+    type: String,
+  },
+  founded: {
+    type: String,
+  },
+  mission: {
+    type: String,
+  },
+  overAllRating: {
+    type: Number,
+    default: 0,
+  },
+  ceoApprovalRating: {
+    type: Number,
+    default: 0,
+  },
+  recommendationRating: {
+    type: Number,
+    default: 0,
+  },
+  logo: {
+    type: String,
+  },
+  featuredreviews: [
+    {
+      date: {
         type: String,
-        required: true
+      },
+      favorite: {
+        type: Boolean,
+      },
+      featured: {
+        type: Boolean,
+      },
+      company: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'company',
+      },
+      student: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'student',
+      },
+      approvalStatus: {
+        type: String,
+      },
+      headline: {
+        type: String,
+      },
+      pros: {
+        type: String,
+      },
+      cons: {
+        type: String,
+      },
+      overAllRating: {
+        type: String,
+      },
+      comment: {
+        type: String,
+      },
+      currentOrFormer: {
+        type: String,
+      },
     },
-    email: {
+  ],
+  interview: [
+    {
+      overallInterviewExp: {
         type: String,
         required: true,
-        unique: true,
-    },
-    profilePic: {
+      },
+      title: {
         type: String,
-        default: 'default'
-    },
-    logo: {
+      },
+      description: {
         type: String,
-        default: 'default'
-    },
-    ceoName: {
+      },
+      jobTitle: {
         type: String,
-    },
-    location: {
+      },
+      difficulty: {
         type: String,
-    },
-    description: {
-        type: String
-    },
-    website: {
+      },
+      offerStatus: {
         type: String,
-    },
-    size: {
-        type: String
-    },
-    type: {
-        type: String
-    },
-    revenue: {
-        type: String
-    },
-    headquarters: {
-        type: String
-    },
-    mission: {
-        type: String
-    },
-    industry: {
-        type: String
-    },
-    founded: {
-        type: String
-    },
-    mission: {
+      },
+      questions: {
         type: String,
+      },
+      answers: {
+        type: String,
+      },
+      date: {
+        type: String,
+        default: Date.now(),
+      },
     },
-    overAllRating: {
+  ],
+  salary: [
+    {
+      baseSalary: {
+        type: String,
+        required: true,
+      },
+      avgTotalPay: {
+        type: String,
+      },
+      bonuses: {
+        type: String,
+      },
+      jobTitle: {
+        type: String,
+      },
+      yearsOfExperience: {
+        type: String,
+      },
+      location: {
+        type: String,
+        required: true,
+      },
+      salaryGender: {
+        type: String,
+      },
+    },
+  ],
+  photos: [
+    {
+      file: {
+        type: String,
+      },
+      status: {
+        type: String,
+        default: 'new',
+      },
+    },
+  ],
+  date: { type: Date, default: Date.now() },
+  views: [
+    {
+      date: {
+        type: String,
+      },
+      count: {
         type: Number,
-        default: 0
+      },
     },
-    ceoApprovalRating: {
-        type: Number,
-        default: 0
-    },
-    recommendationRating: {
-        type: Number,
-        default: 0
-    },
-    logo: {
-        type: String
-    },
-    featuredreviews: [{
-        date: {
-            type: String
-        },
-        favorite: {
-            type: Boolean
-        },
-        featured: {
-            type: Boolean
-        },
-        company: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'company',
-        },
-        student: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'student',
-        },
-        approvalStatus: {
-            type: String
-        },
-        headline: {
-            type: String
-        },
-        pros: {
-            type: String
-        },
-        cons: {
-            type: String
-        },
-        overAllRating: {
-            type: String
-        },
-        comment: {
-            type: String
-        },
-        currentOrFormer: {
-            type: String
-        }
-    }],
-    interview: [{
-        overallInterviewExp: {
-            type: String,
-            required: true
-        },
-        title: {
-            type: String
-        },
-        description: {
-            type: String
-        },
-        jobTitle: {
-            type: String
-        },
-        difficulty: {
-            type: String
-        },
-        offerStatus: {
-            type: String
-        },
-        questions: {
-            type: String
-        },
-        answers: {
-            type: String
-        },
-        date: {
-            type: String,
-            default: Date.now()
-        }
-    }],
-    salary: [{
-        baseSalary: {
-            type: String,
-            required: true
-        },
-        avgTotalPay: {
-            type: String
-        },
-        bonuses: {
-            type: String
-        },
-        jobTitle: {
-            type: String
-        },
-        yearsOfExperience: {
-            type: String
-        },
-        location: {
-            type: String,
-            required: true
-        },
-        salaryGender: {
-            type: String
-        }
-    }],
-    date: { type: Date, default: Date.now() },
-    views: [{
-        date: {
-            type: String
-        },
-        count: {
-            type: Number
-        }
-    }],
+  ],
 });
 
 const Company = mongoose.model('company', CompanySchema);
--- backend/routes/student/landingPage.js
+++ backend/routes/student/landingPage.js
@@ -7,6 +7,7 @@ const Student = require('../../models/StudentModel');
 const Company = require('../../models/CompanyModel');
 const Jobposting = require('../../models/JobPostingModel');
 const Review = require('../../models/ReviewModel');
+const e = require('express');
 
 router.post('/', checkAuth, async (req, res) => {
   console.log('here: ', req.user);
@@ -37,6 +38,7 @@ router.get('/search/:data/:query', async (req, res) => {
   const query = req.params.query;
   try {
     let results = [];
+    let interviewCount, salaryCount;
     if (query === 'JOBS') {
       results = await Jobposting.find({
         title: { $regex: `.*${searchData}.*` },
@@ -46,12 +48,65 @@ router.get('/search/:data/:query', async (req, res) => {
         name: { $regex: `.*${searchData}.*` },
       });
     }
+
     // reviews = await Review.find({})
     if (results.length === 0) {
       return res.status(400).json({ errors: [{ msg: 'No results found.' }] });
     }
 
-    res.status(200).json(results);
+    // let reviewCount;
+    // const counts = results.map(async (each) => {
+    //   if (query === 'JOBS') {
+    //     reviewCount = await Review.find({
+    //       company: each.company._id,
+    //     }).count();
+    //     return reviewCount;
+    //   } else {
+    //     reviewCount = await Review.find({ company: each._id }).count();
+    //     return reviewCount;
+    //   }
+    // });
+
+    // const data = {
+    //   results,
+    //   counts,
+    // };
+
+    // console.log(data);
+
+    const ids = results.map((each) => {
+      if (query === 'JOBS') {
+        return each.company._id.toString();
+      } else {
+        return each._id.toString();
+      }
+    });
+
+    console.log('id: ', ids);
+
+    const ans = await Review.aggregate([
+      {
+        $group: {
+          _id: '$company',
+          company_id: {
+            $first: '$company',
+          },
+          count: { $sum: 1 },
+        },
+      },
+    ]);
+
+    const final = ans.filter((each) =>
+      ids.includes(each.company_id.toString()),
+    );
+
+    const data = {
+      results,
+      final,
+    };
+    // console.log(data);
+
+    res.status(200).json(data);
   } catch (err) {
     console.log(err);
     res.status(500).send('Server Error');
