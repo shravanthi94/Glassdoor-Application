3de1c1f634205c5c0b3f6cda05d215219d14723c
AnudeepCHS <srinivasanudeep.chitta@sjsu.edu>
2020-12-02 14:43:43 -0800

    component Applications

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -57,12 +57,15 @@ import CompanyJobDetails from './Students/CompanyJobDetails';
 import AddPhotos from './Students/AddPhotos';
 import JobHomePage from './Students/JobsHomePage';
 // import JobsNav from './Students/JobsNav';
-
+import StudentJobs from './Students/StudentJobs'
 // Student - Contributions
 import StudentReviews from './Students/contributions/StudentReviews';
 import StudentSalaries from './Students/contributions/StudentSalaries';
 import StudentInterviews from './Students/contributions/StudentInterviews';
 import StudentPhotos from './Students/contributions/StudentPhotos';
+
+import StudentApplications from './Students/StudentApplications';
+
 
 import setAuthToken from '../helpers/setAuthToken';
 import { history } from '../helpers/history';
@@ -136,6 +139,7 @@ class Main extends Component {
               path='/student/update/jobPreference'
               component={JobPreference}
             />
+
             <PrivateRoute
               exact
               path='/student/update/demographics'
@@ -153,6 +157,11 @@ class Main extends Component {
             />
             <PrivateRoute
               exact
+              path='/student/applications'
+              component={StudentApplications}
+            />
+            <PrivateRoute
+              exact
               path='/student/contributions/interviews'
               component={StudentInterviews}
             />
@@ -161,7 +170,7 @@ class Main extends Component {
               path='/student/contributions/photos'
               component={StudentPhotos}
             />
-            {/* <PrivateRoute exact path='/student/jobs' component={JobsNav} /> */}
+             <PrivateRoute exact path='/student/StudentJobs' component={StudentJobs} />
             <PrivateRoute
               exact
               path='/companydashboard'
