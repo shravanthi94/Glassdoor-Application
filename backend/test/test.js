93f527d27057566906190af00f5060020fe60ac8
Shravanthi <57304916+shravanthi94@users.noreply.github.com>
2020-12-03 14:42:47 -0800

    Fixed mocha test results

--- backend/test/test.js
+++ backend/test/test.js
@@ -1,4 +1,4 @@
-const app = require('../app');
+const app = require('../index');
 const chai = require('chai');
 chai.use(require('chai-http'));
 const { expect } = require('chai');
@@ -23,7 +23,7 @@ it('Company Signup Test', () => {
     .post('/company/signup')
     .send({
       name: 'Netflix',
-      email: 'netflix@gmail.com',
+      email: 'netflix1@gmail.com',
       password: 'test',
     })
     .end(function (err, res) {
@@ -37,32 +37,38 @@ it('Current Student Profile Test', () => {
     .get('/student/landing')
     .set(
       'x-auth-token',
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNWZiNDM3ODc2YzQ4NWE1MTkwOGVjODQwIiwibmFtZSI6IlNocmF2YW50aGkiLCJlbWFpbCI6InNocmFAbWFpbC5jb20iLCJ1c2VydHlwZSI6InN0dWRlbnQifSwiaWF0IjoxNjA3MDMyNzA2LCJleHAiOjE2MTMwMzI3MDZ9.dw9Y4FbBSiper-Fk_U3mkX9nMVYne1QuPp7m0RRUiBA',
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNWZiNDM3ODc2YzQ4NWE1MTkwOGVjODQwIiwibmFtZSI6IlNocmF2YW50aGkiLCJlbWFpbCI6InNocmFAbWFpbC5jb20iLCJ1c2VydHlwZSI6InN0dWRlbnQifSwiaWF0IjoxNjA3MDM0MzA1LCJleHAiOjE2MTMwMzQzMDV9.UFDeILyQZP7QM5M5IcfJacDaGxhO6nT-_ps7aNxiuR4',
     )
     .end(function (err, res) {
       expect(res).to.have.status(200);
     });
 });
 
-it('Update student profile test', () => {
+it('Update Student Profile Test', () => {
   chai
     .request(url)
     .post('/student/profile/basic')
-    .set('x-auth-token', '')
+    .set(
+      'x-auth-token',
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNWZiNDM3ODc2YzQ4NWE1MTkwOGVjODQwIiwibmFtZSI6IlNocmF2YW50aGkiLCJlbWFpbCI6InNocmFAbWFpbC5jb20iLCJ1c2VydHlwZSI6InN0dWRlbnQifSwiaWF0IjoxNjA3MDM0MzA1LCJleHAiOjE2MTMwMzQzMDV9.UFDeILyQZP7QM5M5IcfJacDaGxhO6nT-_ps7aNxiuR4',
+    )
     .send({
-      name: 'Sam Jonas',
-      email: 'sam.jonas@gmail.com',
+      name: 'Shravanthi S Bhoopalam',
+      email: 'shra@gmail.com',
     })
     .end(function (err, res) {
       expect(res).to.have.status(200);
     });
 });
 
-it('Admin get all new reviews to approve', () => {
+it('Admin Get Gll New Reviews To Approve', () => {
   chai
     .request(url)
     .get('/admin/review/newreviews')
-    .set('x-auth-token', '')
+    .set(
+      'x-auth-token',
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbiI6eyJpZCI6MSwibmFtZSI6IkJoYXZhbmEiLCJlbWFpbCI6ImJoYXZhbmFAZ21haWwuY29tIiwidXNlcnR5cGUiOiJhZG1pbiJ9LCJpYXQiOjE2MDcwMzQzNTUsImV4cCI6MTYxMzAzNDM1NX0.t4m-iYvL2dCxxFhoj-I5Ct09r7rd4WHTzwV_90TkSr4',
+    )
     .end(function (err, res) {
       expect(res).to.have.status(200);
     });
